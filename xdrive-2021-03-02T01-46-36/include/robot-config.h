8efe8425a54a14908fbaa2eb2ffb32c3849e16c3
mlie5r <michelleli455@gmail.com>
2021-03-01 20:48:16 -0500

    xdrive
    added one for controller testing and one w a parameter so u can choose which direction to go if thats helpful

--- xdrive-2021-03-02T01-46-36/include/robot-config.h
+++ xdrive-2021-03-02T01-46-36/include/robot-config.h
@@ -3,16 +3,13 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
+extern motor RF;
+extern motor RB;
+extern motor LF;
+extern motor LB;
 extern controller Controller1;
-extern motor Motor1;
-extern motor Motor2;
-extern motor Motor3;
 extern motor Motor8;
 extern motor Motor9;
-extern motor Motor16;
-extern motor Motor17;
-extern motor Motor18;
-extern motor Motor19;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- xdrive-2021-03-02T01-46-36/src/robot-config.cpp
+++ xdrive-2021-03-02T01-46-36/src/robot-config.cpp
@@ -8,16 +8,13 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
+motor RF = motor(PORT12, ratio18_1, false);
+motor RB = motor(PORT14, ratio18_1, false);
+motor LF = motor(PORT18, ratio18_1, false);
+motor LB = motor(PORT13, ratio18_1, false);
 controller Controller1 = controller(primary);
-motor Motor1 = motor(PORT1, ratio18_1, false);
-motor Motor2 = motor(PORT2, ratio18_1, false);
-motor Motor3 = motor(PORT3, ratio18_1, true);
-motor Motor8 = motor(PORT8, ratio18_1, true);
-motor Motor9 = motor(PORT9, ratio6_1, false);
-motor Motor16 = motor(PORT16, ratio18_1, false);
-motor Motor17 = motor(PORT17, ratio18_1, false);
-motor Motor18 = motor(PORT18, ratio18_1, true);
-motor Motor19 = motor(PORT19, ratio18_1, true);
+motor Motor8 = motor(PORT8, ratio18_1, false);
+motor Motor9 = motor(PORT9, ratio18_1, true);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
