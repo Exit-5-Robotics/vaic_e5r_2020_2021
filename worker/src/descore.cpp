08670ab75090ab17704f333278cf4c16df84a634
SarcasticSnail <54252089+SarcasticSnail@users.noreply.github.com>
2021-03-20 19:15:11 -0400

    idrk wat we did lol

--- worker/src/descore.cpp
+++ worker/src/descore.cpp
@@ -13,25 +13,21 @@ bool checkDescore(){
         highestBox[0] = local_map.boxobj[i].y;
         highestBox[1] = i;
       }
-    }
-    Brain.Screen.printAt( 10, 200, "highestBox    %d", highestBox[1] );
-
-    Brain.Screen.printAt(10, 260, "color    %s", local_map.mapobj[highestBox[1]].classID);
+    }    
     
     if(local_map.mapobj[highestBox[1]].classID != OUR_COLOR){
-        driveAutoDist(0, 300); //move towards tower
       return true;
-    }else{
-      driveAutoDist(1, 100); //move backwards - can change movement later
     }
   }
-  
+  driveAutoDist(1, 1000);
   return false;
 }
 
 
+
+
 void descore (){
   if(checkDescore()){
-    
+    driveAutoDist(0, 10);
   }
 }
