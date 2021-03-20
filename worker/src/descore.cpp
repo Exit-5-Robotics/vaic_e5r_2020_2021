6c2bf1c4872680c3a8a82c48cd95f03fbf863d90
SarcasticSnail <54252089+SarcasticSnail@users.noreply.github.com>
2021-03-20 19:29:11 -0400

    Update descore.cpp

--- worker/src/descore.cpp
+++ worker/src/descore.cpp
@@ -2,15 +2,27 @@
 // call descore() to check if descoring is necessary / descore
 
 bool checkDescore(){
+  int sum0 = 0, sum1 = 0, sum2 = 0;
   static MAP_RECORD  local_map;
+
   jetson_comms.get_data( &local_map );
 
   if(local_map.boxnum >= 3){
+    int avgY[local_map.boxnum] = {};
+    for(int k = 0; k < local_map.boxnum; k++){
+      for(int i = 0; i < 15; i++){
+        static MAP_RECORD  local_map;
+        avgY[k] += local_map.boxobj[k].y;
+      }
+      avgY[k] /= 15;
+    }
+    
+
     //{y pos, id}
-    int highestBox[] = {local_map.boxobj[0].y, 0}; 
+    int highestBox[] = {avgY[0], 0}; 
     for(int i = 1; i < local_map.boxnum; i++){
-      if (local_map.boxobj[i].y > highestBox[0]){
-        highestBox[0] = local_map.boxobj[i].y;
+      if (avgY[i] > highestBox[0]){
+        highestBox[0] = avgY[i];
         highestBox[1] = i;
       }
     }    
