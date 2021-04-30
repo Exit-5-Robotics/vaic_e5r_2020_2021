1dcb474ff22a8a1ee0769cd50a20f19bc8d01fbd
alame5r <alam.e5r@gmail.com>
2021-04-29 20:52:55 -0400

    towerMap

---
+++ worker/include/towerMap.h
@@ -0,0 +1,21 @@
+#include "ai_jetson.h"
+
+extern bool testChange( void );
+extern float round( float var );
+extern std::string arrToString( int (&arr)[9] );
+extern int intLen( int n );
+
+extern int stringToX( std::string pos );
+extern int stringToY( std::string pos );
+
+extern std::string positionToString( int x_pos, int y_pos );
+extern std::string getBallPosition( fifo_object_box boxObj );
+
+extern void cacheGoals( void );
+extern void loadGoalsInfo(const char *message, const char *linkname, int32_t index, double value);
+extern int getGoal( int x_pos, int y_pos );
+
+extern std::string getClosest( void );
+extern std::string getClosestOurColor( void );
+extern int getClosestGoal( void );
+extern void receiveMessages( void );
--- worker/src/main.cpp
+++ worker/src/main.cpp
@@ -39,6 +39,8 @@ competition Competition;
 // data from the Jetson nano
 //
 ai::jetson  jetson_comms;
+float local_x, local_y, local_heading;
+MAP_RECORD       local_map;
 
 /*----------------------------------------------------------------------------*/
 // Create a robot_link on PORT1 using the unique name robot_32456_1
@@ -58,8 +60,6 @@ ai::robot_link link( PORT11, "robot_3063_1", linkType::manager );
 #pragma message("building for the worker")
 ai::robot_link link( PORT11, "robot_3063_1", linkType::worker );
 #endif
-
-static MAP_RECORD       local_map;
 
 /*----------------------------------------------------------------------------*/
 
@@ -190,6 +190,13 @@ int main() {
     while(1) {
         // get last map data
         jetson_comms.get_data( &local_map );
+        local_x = local_map.pos.x/25.4;
+        local_y = local_map.pos.y/25.4;
+        local_heading = local_map.pos.az*180/M_PI + 180;
+        Brain.Screen.printAt(10, 40, "%.2f %.2f %.2f", local_x, local_y, local_heading);
+        cacheGoals();
+        Brain.Screen.printAt(10, 60, "%d %d %d %d %d %d %d %d %d", mapScore[0], mapScore[1], mapScore[2], 
+          mapScore[3], mapScore[4], mapScore[5], mapScore[6], mapScore[7], mapScore[8]);
 
         // set our location to be sent to partner robot
         link.set_remote_location( local_map.pos.x, local_map.pos.y, local_map.pos.az );
